a072ae14d4f6558a40a1eec5e4ad717efb7459a1
Todd Perry <up612136@myport.ac.uk>
2016-08-31 10:09:59 +0100

    Update NBO.py

--- NBO.py
+++ NBO.py
@@ -122,6 +122,7 @@ class NBO(optimizer.Optimizer):
             if npy < 0:
                 npy = 0
                 nvy = -vy*hy.cor
+            self.velocities[i] = (nvy, nvy)
             self.positions[i] = (npx, npy)
 
     def step(self):
